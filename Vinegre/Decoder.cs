6c0b7260d7739cdbe3cbb959ee3cbaccdfdf68e6
fzabnicki <63243182+fzabnicki@users.noreply.github.com>
2021-02-19 20:01:14 +0100

    Final version

---
+++ Vinegre/Decoder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Vinegre
+{
+    public class Decoder
+    {
+        string alphabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSŚTUVWXYZŹŻ";
+        public string Algorythm(string input, string key, bool decode)
+        {
+            string output = String.Empty;
+
+            input = InputUnification(input);
+            key = InputUnification(key);
+
+            if (input.Length > key.Length)
+                key = MakeTheSameLenght(key, input.Length);
+            else if (input.Length < key.Length)
+                input = MakeTheSameLenght(input, key.Length);
+
+            switch (decode)
+            {
+                case true:
+                    for (int i = 0; i < input.Length; i++)
+                        output += alphabet[(alphabet.IndexOf(input[i]) + alphabet.IndexOf(key[i])) % alphabet.Length];
+                    break;
+                case false:
+                    for (int i = 0; i < input.Length; i++)
+                        output += alphabet[(alphabet.IndexOf(input[i]) - alphabet.IndexOf(key[i]) + alphabet.Length) % alphabet.Length];
+                    break;
+                default:
+                    break;
+            }
+            return output;
+        }
+
+        private string MakeTheSameLenght(string input, int length)
+        {
+            string output = String.Empty;
+            for (int i = 0; i < length; i++)
+                output += input[i % (input.Length)];
+            return output;
+        }
+
+        public string InputUnification(string input)
+        {
+            string temporary = String.Empty;
+            input = input.ToUpper();
+            foreach (char c in input)
+            {
+                if (alphabet.Contains(c))
+                    temporary += c;
+            }
+            return temporary;
+        }
+    }
+}
---
+++ Vinegre/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace Vinegre
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void actionButton_Click(object sender, RoutedEventArgs e)
+        {
+            Decoder d = new Decoder();
+            if (codeRadioBtn.IsChecked == true)
+                outputTextBox.Text = d.Algorythm(inputTextBox.Text, keyTextBox.Text, true);
+            else if(decodeRadioBtn.IsChecked == true)
+                outputTextBox.Text = d.Algorythm(inputTextBox.Text, keyTextBox.Text, false);
+            else
+                MessageBox.Show("Wybierz rodzaj operacji.");
+        }
+    }
+}
